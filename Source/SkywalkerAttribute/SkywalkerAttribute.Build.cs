2a672528ae2bb76e3b6ac6f8351833d2fca6fb15
Shyfan <410713847@qq.com>
2024-07-20 16:51:39 +0800

    增加属性

--- Source/SkywalkerAttribute/SkywalkerAttribute.Build.cs
+++ Source/SkywalkerAttribute/SkywalkerAttribute.Build.cs
@@ -38,6 +38,7 @@ public class SkywalkerAttribute : ModuleRules
 				"Engine",
 				"Slate",
 				"SlateCore",
+                "GameplayAbilities",
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);

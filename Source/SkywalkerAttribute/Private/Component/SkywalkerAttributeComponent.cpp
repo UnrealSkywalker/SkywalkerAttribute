1a72b7c42837252c1d076b39177801432359e60f
Shyfan <410713847@qq.com>
2024-11-07 22:29:27 +0800

    保存

--- Source/SkywalkerAttribute/Private/Component/SkywalkerAttributeComponent.cpp
+++ Source/SkywalkerAttribute/Private/Component/SkywalkerAttributeComponent.cpp
@@ -17,22 +17,7 @@ void USkywalkerAttributeComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// 获取 AbilitySystemComponent，并绑定 AttributeSet
-	if (GetOwner())
-	{
-		AbilitySystemComponent = Cast<UAbilitySystemComponent>(GetOwner()->GetComponentByClass(UAbilitySystemComponent::StaticClass()));
-		if (AbilitySystemComponent)
-		{
-			for (TSubclassOf<USkywalkerAttributeSet> AttributeSetClass : AttributeSetClassArray)
-			{
-				USkywalkerAttributeSet *AttributeSet = NewObject<USkywalkerAttributeSet>(GetOwner(), AttributeSetClass);
-				if (AttributeSet)
-				{
-					AbilitySystemComponent->InitStats(AttributeSet);
-				}
-			}
-		}
-	}
+	// ...
 }
 
 // Called every frame
